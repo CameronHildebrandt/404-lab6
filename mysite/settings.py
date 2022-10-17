5f928daa2b423a5e4b06491be85488b14c8501d4
= <childebr@ualberta.ca>
2022-10-17 17:32:31 -0600

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 import django_on_heroku # top of the file
 
+# comment
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
